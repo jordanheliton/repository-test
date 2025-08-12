b3d9a50ac46e6df14179f5ff3d0a385c526694cc
jordanheliton <jordan.helitonribeiro@gmail.com>
2025-08-12 19:44:03 -0300

    Subindo fonts

--- ConsoleAppTeste/Program.cs
+++ ConsoleAppTeste/Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
 Console.WriteLine("Teste computador novo!");
-
-=======
-Console.WriteLine("Teste computador antigo!");
->>>>>>> d4cb28b08ced8d9937fe658c26b4f5686281d66d
